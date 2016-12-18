a8ba9f7413db7dae11efd984ab0df2dad3230856
mcfetridgem <matthew.mcfetridge@gmail.com>
2016-12-18 18:27:06 +1300

    Update sketch.js

--- processing/sketch.js
+++ processing/sketch.js
@@ -1,12 +1,59 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/AaGK-fj-BAM
+
+var s;
+var scl = 20;
+
+var food;
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(51);
+
+  if (s.eat(food)) {
+    pickLocation();
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  s.death();
+  s.update();
+  s.show();
+
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+
+
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
 }
