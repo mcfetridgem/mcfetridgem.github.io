2388157368e3aab69ce9ebc6681dc0ba582c62ec
mcfetridgem <matthew.mcfetridge@gmail.com>
2016-12-18 18:31:34 +1300

    Update sketch.js

--- processing/sketch.js
+++ processing/sketch.js
@@ -9,7 +9,8 @@ var scl = 20;
 var food;
 
 function setup() {
-  createCanvas(600, 600);
+  //createCanvas(600, 600);
+  createCanvas(windowWidth,windowHeight);
   s = new Snake();
   frameRate(10);
   pickLocation();
